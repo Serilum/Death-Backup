7cd7f6b134b0a930985bbd50d4d7450a86310eb3
Rick South <rick@natamus.com>
2023-12-12 10:32:35 +0100

    Death Backup source code for 1.20.0.

---
+++ Common/src/main/java/com/natamus/deathbackup/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.deathbackup.util;
+
+public class Reference {
+	public static final String MOD_ID = "deathbackup";
+	public static final String NAME = "Death Backup";
+	public static final String VERSION = "3.0";
+	public static final String ACCEPTED_VERSIONS = "[1.20]";
+}
