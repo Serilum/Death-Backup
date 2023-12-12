25179431adf14bf23469b3eb247e4e77310a6880
Rick South <rick@natamus.com>
2023-12-12 10:32:57 +0100

    Death Backup source code for 1.20.4.

---
+++ Forge/src/main/java/com/natamus/deathbackup/forge/config/IntegrateForgeConfig.java
@@ -0,0 +1,16 @@
+package com.natamus.deathbackup.forge.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.deathbackup.util.Reference;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+
+public class IntegrateForgeConfig {
+	public static void registerScreen(ModLoadingContext modLoadingContext) {
+		modLoadingContext.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> {
+			return new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> {
+				return DuskConfig.DuskConfigScreen.getScreen(screen, Reference.MOD_ID);
+			});
+		});
+	}
+}
