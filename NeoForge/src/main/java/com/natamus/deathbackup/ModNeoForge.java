f979ce64e1b7c3f29e0e38f399fdebed4a53b372
Rick South <rick@natamus.com>
2024-07-28 07:37:20 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/deathbackup/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/deathbackup/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.deathbackup;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.deathbackup.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.deathbackup.neoforge.events.NeoForgeDeathBackupEvent;
 import com.natamus.deathbackup.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
