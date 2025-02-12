81e3daa25a381ec45ec9a47750616d208356a3ec
Rick South <rick@natamus.com>
2025-02-12 23:38:13 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/deathbackup/forge/events/ForgeDeathBackupEvent.java
+++ Forge/src/main/java/com/natamus/deathbackup/forge/events/ForgeDeathBackupEvent.java
@@ -10,17 +10,15 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeDeathBackupEvent {
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandDeathBackup.register(e.getDispatcher());
     }
 
 	@SubscribeEvent
-	public void onPlayerDeath(LivingDeathEvent e) {
+	public static void onPlayerDeath(LivingDeathEvent e) {
 		Entity entity = e.getEntity();
 		Level level = entity.level();
 		if (level.isClientSide) {
