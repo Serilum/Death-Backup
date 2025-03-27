f79183d1dbc1b6629757aca70ab63c20ec083bc6
Rick South <rick@natamus.com>
2025-03-28 00:54:35 +0100

    Death Backup source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/deathbackup/forge/events/ForgeDeathBackupEvent.java
@@ -0,0 +1,34 @@
+package com.natamus.deathbackup.forge.events;
+
+import com.natamus.deathbackup.cmds.CommandDeathBackup;
+import com.natamus.deathbackup.events.DeathBackupEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeDeathBackupEvent {
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	CommandDeathBackup.register(e.getDispatcher());
+    }
+
+	@SubscribeEvent
+	public static void onPlayerDeath(LivingDeathEvent e) {
+		Entity entity = e.getEntity();
+		Level level = entity.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		if (!(entity instanceof Player)) {
+			return;
+		}
+
+		DeathBackupEvent.onPlayerDeath((ServerLevel)level, (ServerPlayer)entity);
+	}
+}
